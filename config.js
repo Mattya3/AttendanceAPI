8230325f99ebd3bcf1cc69d3f2ae6e4c32b42e1e
Mattya3 <115437964+Mattya3@users.noreply.github.com>
2026-04-25 16:34:08 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.ATTEND_CONFIG = {
   liffId: "2009771645-vBqLJXFu",
-  gasEndpoint: "https://script.google.com/macros/s/AKfycbyi4sjjtGyTUO7pVBdSq8bT026plKXNa53t1T2SzuWW9CzFSUw8ggZ7SlP3c0Fr9A/exec"
+  gasEndpoint: "https://script.google.com/macros/s/AKfycbyi4sjjtGyTUO7pVBdSq8bT026plKXNa53t1T2SzuWW9CzFSUw8ggZ7SlP3c0Fr9A/exec",
   // Debug only: true でエラー表示に [E_...] コードを付与
   debugErrorMode: true
 };
