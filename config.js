bce90a62891da8d4425292ce011bbeebb1ca6b5c
Mattya3 <115437964+Mattya3@users.noreply.github.com>
2026-04-25 18:26:34 +0900

    Add calendar ID and timezone to config

--- config.js
+++ config.js
@@ -2,5 +2,7 @@ window.ATTEND_CONFIG = {
   liffId: "2009771645-vBqLJXFu",
   gasEndpoint: "https://script.google.com/macros/s/AKfycbyi4sjjtGyTUO7pVBdSq8bT026plKXNa53t1T2SzuWW9CzFSUw8ggZ7SlP3c0Fr9A/exec",
   // Debug only: true でエラー表示に [E_...] コードを付与
+  calendarId: "thisistokenaddress@gmail.com",
+  calendarTimeZone: "Asia/Tokyo",
   debugErrorMode: true
 };
