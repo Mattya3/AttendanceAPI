fa64aa089120b456cb146ba35c70ce77feefb7c4
Mattya3 <115437964+Mattya3@users.noreply.github.com>
2026-04-25 15:34:14 +0900

    Add ATTEND_CONFIG with liffId and gasEndpoint

---
+++ config.js
@@ -0,0 +1,4 @@
+window.ATTEND_CONFIG = {
+  liffId: "2009771645-vBqLJXFu",
+  gasEndpoint: "https://script.google.com/macros/s/AKfycbyi4sjjtGyTUO7pVBdSq8bT026plKXNa53t1T2SzuWW9CzFSUw8ggZ7SlP3c0Fr9A/exec"
+};
